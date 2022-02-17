820a8ec1d7acb17c08b2fa3fe94910b775f79db4
rcapeto <raphaelcapeto@gmail.com>
2022-02-17 02:33:25 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+  return(
+    <h1>Hello World</h1>
+  );
+};
+
+export default App;
---
+++ src/typings/vtex.d.ts
@@ -0,0 +1,43 @@
+import { FunctionComponent } from 'react';
+
+declare global {
+   interface VTEXCustomComponent<Props> extends FunctionComponent<Props> {
+      defaultProps?: Props;
+      schema?: Schema;
+   }
+   
+   interface Schema {
+      title: string;
+      type: SchemaType;
+      default?: any;
+      properties: SchemaProperties;
+   }
+}
+
+export type SchemaType = 'array' | 'object' | 'string' | 'boolean' | 'number';
+
+export interface SchemaProperties {
+  __editorItemTitle?: {
+    default: string
+    title: string
+    type: SchemaType
+  },
+   [key: string]: {
+      type: SchemaType;
+      enum?: string[];
+      default?: any;
+      title?: string;
+      description?: string;
+      properties?: SchemaProperties;
+      format?: 'date-time',
+      widget?: {
+         "ui:widget"?: "image-uploader" | "datetime"
+      },
+      items?: {
+         type: SchemaType;
+         title?: string;
+         properties?: SchemaProperties;
+         default?: any;
+      }
+   }
+}
