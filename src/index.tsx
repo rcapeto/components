e6b9f58e6fca8755154ef86df55ff12042256281
rcapeto <raphaelcapeto@gmail.com>
2022-02-17 18:06:12 -0300

    set master in initial state

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './global.scss';
 
-import { ToastProvider } from './components/ToastProvider';
-
 ReactDOM.render(
-  <ToastProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ToastProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
