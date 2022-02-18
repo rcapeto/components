17f8df6c383bdccec227fe64ce2f1c76d908dc0b
rcapeto <raphaelcapeto@gmail.com>
2022-02-18 02:49:09 -0300

    change app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import { FunctionComponent } from 'react';
 
-import { TooltipWrapper } from './components/Tooltip';
-
 const App: FunctionComponent = () => {
   return(
-    <div>
-      <TooltipWrapper label='Tooltip Message'>
-        <h1>Tooltip Example</h1>
-      </TooltipWrapper>
-    </div>
+    <h1>Master</h1>
   );
 };
 
