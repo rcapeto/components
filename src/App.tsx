a1c9492b953bc06478706efefb14603f2e7b1a8f
rcapeto <raphaelcapeto@gmail.com>
2022-02-18 17:25:04 -0300

    clean app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import { FunctionComponent } from 'react';
 
-import { AutoCompleteInput } from './components/InputAutoComplete';
-
-const items = [
-  {
-     label: 'React',
-     value: { tech: 'react'}
-  },
-  {
-    label: 'Typescript',
-    value: { tech: 'Typescript'}
-  },
-]; 
-
 const App: FunctionComponent = () => {
   return(
     <div className="container">
-      <AutoCompleteInput 
-        items={items}
-      />
+      <h1>master</h1>
     </div>
   );
 };
