f581e0be74fb59e485f0bcb7cdf2d858de97ec77
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2022-02-18 01:05:44 -0300

    fix(bannercountdown): adjust contents and styles image

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { ContentBannerProps } from './components/BannerCountdown/types';
 
 const array: ContentBannerProps[] = [
   {
-    image: 'https://media-exp1.licdn.com/dms/image/C5603AQEQ4OoOUdaD-Q/profile-displayphoto-shrink_200_200/0/1643218014648?e=1650499200&v=beta&t=Tt0qZ-IseRkw330JUoTOLKLLPQHrpw9bF8-_YwLPQ-g',
-    alt: 'raphael',
-    title: 'Raphael',
+    image: 'https://www.pngkey.com/png/full/541-5416886_darkness.png',
+    alt: 'darkness',
+    title: 'Darkness',
     countdown:{
       countdownUse:true,
       dataInit:'2022-02-17T08:00:00.000Z',
@@ -14,22 +14,32 @@ const array: ContentBannerProps[] = [
     }
   },
   {
-    image: 'https://www.google.com/logos/doodles/2022/dr-michiaki-takahashis-94th-birthday-6753651837109359-l.webp',
-    alt: 'google',
-    title: 'Google',
+    image: 'https://www.guardiantelecom.com/wp-content/uploads/2020/06/guardian-wave-3-1920x391-1.png',
+    alt: 'guardian',
+    title: 'Guardian',
     countdown:{
       countdownUse:true,
       dataInit:'2022-02-17T08:00:00.000Z',
-      dataFinish:'2022-02-19T03:00:00.000Z'
+      dataFinish:'2022-02-20T03:00:00.000Z'
     }
   },
   {
-    image: 'https://www.google.com/logos/doodles/2022/dr-michiaki-takahashis-94th-birthday-6753651837109359-l.webp',
-    alt: 'google',
-    title: 'Google',
+    image: 'https://www.pngkey.com/png/full/541-5416886_darkness.png',
+    alt: 'darkness',
+    title: 'Darkness',
     countdown:{
       countdownUse:true,
-      dataInit:'2022-02-17T03:00:00.000Z',
+      dataInit:'2022-02-17T08:00:00.000Z',
+      dataFinish:'2022-02-18T03:46:00.000Z'
+    }
+  },
+  {
+    image: 'https://www.guardiantelecom.com/wp-content/uploads/2020/06/guardian-wave-3-1920x391-1.png',
+    alt: 'guardian',
+    title: 'Guardian',
+    countdown:{
+      countdownUse:true,
+      dataInit:'2022-02-17T08:00:00.000Z',
       dataFinish:'2022-02-20T03:00:00.000Z'
     }
   }
