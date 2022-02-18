43a8c73212e9a2418a0f44801cab393e4081213a
rcapeto <raphaelcapeto@gmail.com>
2022-02-18 00:48:05 -0300

    end sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,8 @@
 import { FunctionComponent } from 'react';
 
-import { MdDashboard } from 'react-icons/md';
-import { AiFillSetting, AiOutlinePoweroff } from 'react-icons/ai';
-
-import { Sidebar, SidebarCategory } from './components/Sidebar';
-
-const categories: SidebarCategory[] = [
-  {
-    title: 'My profile',
-    icon: MdDashboard,
-    subcategories: [
-      {
-        icon:AiFillSetting,
-        link: '/profile/settings',
-        title: 'Settings' 
-      },
-      {
-        icon:AiOutlinePoweroff,
-        link: '/logout',
-        title: 'Logout' 
-      }
-    ],
-  },
-];
-
 const App: FunctionComponent = () => {
   return(
-    <header
-      style={{ height: 70, padding: 10, backgroundColor: '#000' }}
-    >
-      <Sidebar 
-        categories={categories}
-        iconColor="#fff"
-        iconSize={20}
-        buttonTitle="Menu"
-      />
-    </header>
+    <h1>Master</h1>
   );
 };
 
