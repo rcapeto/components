4729f9792c04a56409d0810c07a2beeb97f41766
rcapeto <raphaelcapeto@gmail.com>
2022-02-19 02:19:19 -0300

    clean master

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
 import { FunctionComponent } from 'react';
 
-import { NavigationButton } from './components/NavigationButton';
-
 const App: FunctionComponent = () => {
   return(
     <div className="container">
-      <NavigationButton 
-        sectionId="content"
-      />
-
-      <div style={{ height: '150vh' }}/>
-      
-      <div id={'content'}>
-        <h1>Content</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Deleniti nisi, in, excepturi culpa esse architecto quia unde libero doloremque 
-          assumenda ipsa fugit dolore repellat! Reiciendis quo magni eligendi sequi doloremque.
-        </p>
-      </div>
-
-      <div style={{ height: '150vh' }}/>
-
+      <h1>master</h1>
     </div>
   );
 };
