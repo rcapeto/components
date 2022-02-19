298caa84fedabec8ddbb9cdff7a6a67d6778004c
rcapeto <raphaelcapeto@gmail.com>
2022-02-19 02:51:35 -0300

    clean master

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { FunctionComponent } from 'react';
 
-import { AnimatedNumber } from './components/AnimatedNumbers';
-
 const App: FunctionComponent = () => {
   return(
     <div className="container">
-      <div style={{ height: '150vh' }}/>
-      <AnimatedNumber number={12412030}/>
-      <div style={{ height: '150vh' }}/>
-      <AnimatedNumber number={12412030} delay={100}/>
-      <div style={{ height: '150vh' }}/>
+      <h1>master</h1>
     </div>
   );
 };
