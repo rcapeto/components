5c40509a3913f6a10b274f12c82281004050d7e6
rcapeto <raphaelcapeto@gmail.com>
2022-02-20 04:20:26 -0300

    clean master

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { FunctionComponent } from 'react';
-import { BarPercentual } from './components/PercentualBar';
 
 const App: FunctionComponent = () => {
   return(
     <div className="container">
-      {
-        Array.from({ length: 6 }, (_, index) => (
-          <div style={{ height: '200vh' }} key={String(index)}/>
-        ))
-      }
-      <BarPercentual />
+     <h1>master</h1>
     </div>
   );
 };
