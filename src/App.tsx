0006360b004a7162599b06ab18c72e3f05f57933
rcapeto <raphaelcapeto@gmail.com>
2022-02-20 19:00:49 -0300

    clean master

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import { FunctionComponent } from 'react';
-import { Accordion } from './components/Accordion';
-import { FAQ } from './components/FAQ';
 
 const App: FunctionComponent = () => {
-  const items = [
-    {
-      question: 'How many stars this repo deserves?',
-      answer: 'All stars!'
-    }
-  ];
   return(
     <div className="container">
-     <Accordion 
-      title={'How many stars this repo deserves?'}
-      description={'All stars!'}
-     />
-
-     <div style={{ height: '50vh' }}/>
-
-     <FAQ items={items}/>
+      <h1>master</h1>
     </div>
   );
 };
