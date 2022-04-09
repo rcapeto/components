0a22752d97a45c47ca8a27edd66195a76797061d
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2022-04-09 00:11:21 -0300

    feat(useDocumentTitle)Ç finish hook title (09042022)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from 'react';
 import CounterInfo from './components/CounterInfo';
+import { WhatsappChat } from './components/WhatsappChat';
 
    const App: FunctionComponent = () => {
       return(
          <div className="container">
-            <CounterInfo/>
+            <WhatsappChat phone='5516991476318' visible position='center'/>
          </div>
       );
    };
