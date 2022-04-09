c80d5a775f9051ffde139d8f864091e29a311bef
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2022-04-09 14:25:26 -0300

    fix(BannerCountdown): adjust functions and use hook useTime (09042022)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import { FunctionComponent } from 'react';
-import CounterInfo from './components/CounterInfo';
-import { WhatsappChat } from './components/WhatsappChat';
+import { Settings } from 'react-slick';
+import BannerCountdoun from './components/BannerCountdown';
+import { ContentBannerProps } from './components/BannerCountdown/types';
 
-   const App: FunctionComponent = () => {
-      return(
-         <div className="container">
-            <CounterInfo 
-               dataInitial='04/04/2022'
-               counterType='banner'
-               dataFinal='09/04/2022 22:17' 
-            />
-         </div>
-      );
-   };
+const array: ContentBannerProps[] = [
+  {
+    image: 'https://www.google.com/logos/doodles/2022/dr-michiaki-takahashis-94th-birthday-6753651837109359-l.webp',
+    alt: 'google',
+    title: 'Google',
+    countdown:{
+      countdownUse:true,
+      dataInit:'09/04/2022',
+      dataFinish:'09/04/2022 14:16',
+      text:'testooo'
+    }
+  },
+  {
+    image: 'https://www.google.com/logos/doodles/2022/dr-michiaki-takahashis-94th-birthday-6753651837109359-l.webp',
+    alt: 'google',
+    title: 'Google',
+    countdown:{
+      countdownUse:true,
+      dataInit:'09/04/2022',
+      dataFinish:'09/04/2022 15:00',
+      text:'testooo'
+    }
+  },
+];
+
+const setting: Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const App = () => {
+  return (
+    <BannerCountdoun
+      sliderSettings={setting}
+      contents={array}
+    />
+  );
+};
 
 export default App;
--- src/components/BannerCountdown/components/banner/index.tsx
+++ src/components/BannerCountdown/components/banner/index.tsx
@@ -1,11 +1,13 @@
 
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { ContentBannerProps } from "../../types";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import './styles.scss'
-import Countdown from '../Countdown';
+import { useTimer } from '../../../../hooks/useTimer';
+import Loading from '../Loading';
+import { classes } from '../../../../config/class';
 
 const Banner: FunctionComponent<ContentBannerProps> = ({
   image = 'https://howfix.net/wp-content/uploads/2018/02/sIaRmaFSMfrw8QJIBAa8mA-article.png',
@@ -17,13 +19,67 @@ const Banner: FunctionComponent<ContentBannerProps> = ({
     dataInit: '',
     text: '',
   }
-  
 }) => {
+  const [ loading, setLoading] = useState(true)
+  const baseClass = classes.baseG;
+
+  const currentDate = (date: any) => {
+    const [day, month, year] = date.split('/');
+    return ([month, day, year].join('/'));
+  }
+  useEffect(() => {
+    setLoading(false);
+  },[])
+  // const dataInitFormater = currentDate(countdown.dataInit);
+  const dataFinishFormater = currentDate(countdown.dataFinish);
+
+  const { secondsSTR, minutesSTR, hoursSTR, active } = useTimer(dataFinishFormater);
+  
+  if (loading) {
+    return (
+      <div
+        className={`${baseClass}countDownBannerContainer`}
+        id="banner-countdown"
+      >
+        <Loading />
+      </div>
+    )
+  }
 
   return (
     <div className='gbcintra-component--box-image'>
-      {countdown.countdownUse && (
-        <Countdown {...countdown} />
+      {(countdown.countdownUse && active) && (
+        <div className={`${baseClass}countDownBannerCounterWrap`}>
+          <h2 className={`${baseClass}countDownBannerTitle`}>
+            {countdown.text}
+          </h2>
+          <ul className={`${baseClass}bannerCountDownBFCounter`}>
+            <li className={`${baseClass}bannerCountDownBFCounterBox`}>
+              <div className={`${baseClass}bannerCountDownBFCounterItem`}>
+                {hoursSTR}
+              </div>
+              <div className={`${baseClass}bannerCountDownBFCounterText`}>
+                Horas
+              </div>
+            </li>
+            <li className={`${baseClass}bannerCountDownBFCounterBox`}>
+              <div className={`${baseClass}bannerCountDownBFCounterItem`}>
+                {minutesSTR}
+              </div>
+              <div className={`${baseClass}bannerCountDownBFCounterText`}>
+                Minutos
+              </div>
+            </li>
+            <li className={`${baseClass}bannerCountDownBFCounterBox`}>
+              <div className={`${baseClass}bannerCountDownBFCounterItem`}>
+                {secondsSTR}
+              </div>
+              <div className={`${baseClass}bannerCountDownBFCounterText`}>
+                Segundos
+              </div>
+            </li>
+          </ul>
+        </div>
       )}
       <figure className='gbcintra-component--figure-banner'>
         <img className='gbcintra-component--image-banner' src={image} alt={alt} title={title} />
