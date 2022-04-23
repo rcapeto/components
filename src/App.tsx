1a90e6d94110a78e6cb72249d22571df0bdc9c49
rcapeto <raphaelcapeto@gmail.com>
2022-04-23 14:16:53 -0300

    clean app index

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import { useRange } from "./hooks/useRange";
-
 const App = () => {
-  const { createRange, createRangeWithStep } = useRange();
-
-  const r1 = createRange(3); 
-  const r2 = createRangeWithStep(10, 0, 5); 
-
-  console.log(r1); //[1, 2, 3]
-  console.log(r1); //[0, 5, 10]
- 
   return(
-    <h1>Example</h1>
+    <h1>Master</h1>
   )
 };
 
