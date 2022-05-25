d72f1c8fdbcb280c0a4320125e6f41166042935b
rcapeto <raphaelcapeto@gmail.com>
2022-05-24 21:32:23 -0300

    update useCookie hook

--- src/hooks/useCookies/index.tsx
+++ src/hooks/useCookies/index.tsx
@@ -1,15 +1,57 @@
 import { useEffect, useState } from 'react';
 
-import { Cookies } from './types';
-
-//pode ser assim
-//record<1,2> => 1. key type, 2. value type
 type Cookie = Record<string, string>;
 
-export const useCookies = () => {
-   const [cookies, setCookies] = useState<Cookies>();
+interface CookieConfig {
+   expireDays?: number;
+   path?: string;
+}
 
-   useEffect(() => {
+interface FormatedCookieConfig {
+   expireDays?: string;
+   path?: string;
+   key: string;
+   value?: string;
+   'Max-Age'?: string;
+};
+
+export const useCookies = () => {
+   const [cookies, setCookies] = useState<Cookie>();
+
+   const setCookie = (key: string, value: string, config?: CookieConfig) => {
+      const formatPath = (config && config.path) ?? '/';
+      const domain = window.location.hostname;
+
+      const state = { 
+         domain,
+         path: formatPath,
+         key: encodeURIComponent(key),
+         expires: '',
+         value
+      };
+
+      if(config && config.expireDays) {
+         const date = new Date();
+         date.setDate(date.getDate() + config.expireDays);
+
+         state.expires = date.toUTCString();
+      }
+
+      const cookie = formatedCookie(state);
+      document.cookie = cookie;
+   };
+
+   const getCookie = (key: string) => {
+      if(!cookies) return;
+
+      for(const cookieKey of Object.keys(cookies)) {
+         if(cookieKey === key) {
+            return cookies[cookieKey];
+         }
+      }
+   };
+
+   const getAll = () => {
       const navigatorCookies: Cookie = 
          document.cookie.split(';')
             .map(cookie => cookie.split('='))
@@ -17,10 +59,60 @@ export const useCookies = () => {
                ...acc,
                [key.trim()]: values.join('')
             }), {});
-      setCookies(navigatorCookies);
+      return navigatorCookies;
+   };
+
+   //para remover precisa ter o mesmo path.
+   const removeCookie = (key: string, config?: CookieConfig) => {
+      const formatPath = (config && config.path) ?? '/';
+      const domain = window.location.hostname;
+
+      const state = { 
+         domain,
+         path: formatPath,
+         'Max-Age': '-99999999',
+         key: encodeURIComponent(key),
+         expires: '',
+      };
+
+      if(config && config.expireDays) {
+         const date = new Date();
+         date.setDate(date.getDate() + config.expireDays);
+
+         state.expires = date.toUTCString();
+      }
+
+      const cookie = formatedCookie(state);
+      document.cookie = cookie;
+   } 
+
+   const formatedCookie = (config: FormatedCookieConfig) => {
+      let str: string = '';
+
+      for(const [key, value] of Object.entries(config)) {
+         if(value && key !== 'key' && key !== 'value') {
+            str += ` ${key}=${value};`
+            console.log(key, value)
+         }
+      }
+
+      if(config.value) {
+         return `${config.key}=${config.value};${str}`;
+      } else {
+         return `${config.key}=;${str}`;
+      }
+
+   };
+
+   useEffect(() => {
+      setCookies(getAll());
    }, []);
 
    return {
       cookies,
+      setCookie,
+      getCookie,
+      getAll,
+      removeCookie
    }
 };
