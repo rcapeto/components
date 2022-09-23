c335fdeb35ff487473c4c5b69086f5ea0038f0fd
rcapeto <raphaelcapeto@gmail.com>
2022-09-23 13:23:39 -0300

    reset app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,5 @@
-import { Form, PhoneCheck } from './components/Form';
-
-const App = () => {
+export default function App() {
   return(
-    <>
-      <h1>Register Form</h1>
-      <Form 
-        style={{ padding: '10px 30px'}}
-        handleSubmitForm={(fields, event) => {
-          console.log({ fields, event })
-        }}
-      >
-        
-        <Form.Input 
-          label='Name'
-          inputName='name'
-        />
-
-        <Form.Input 
-          label='Last Name'
-          inputName='last_name'
-        />
-
-        <Form.Input 
-          label='Phone'
-          inputName='phone'
-          validation={PhoneCheck}
-          inlineValidation
-          mask='cellphone'
-          errorMessage='Invalid Phone'
-        />
-
-        <Form.SubmitButton btnText="Register" />
-      </Form>
-    </>
+    <h1>Master</h1>
   );
 }
-
-export default App;
