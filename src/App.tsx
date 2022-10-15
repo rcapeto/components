4ea527074c9b9d58ae0e6d3cb1d537073fc40dc8
rcapeto <raphaelcapeto@gmail.com>
2022-10-15 20:59:15 -0300

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
-import { FaPhone, FaUser } from 'react-icons/fa';
 import { TestLayout } from './components/TestLayout';
-import { Form } from './lib/Form';
-import { MaskInput } from './utils/masks';
-import { PhoneCheck } from './utils/validations';
 
 export default function App() {
   return(
     <TestLayout isDarkMode>
-      <div style={{ width: '100%', maxWidth: 420 }}>
-        <Form.Wrapper onHandleSubmit={(inputs, error) => {}}>
-          <Form.Title>Register User</Form.Title>
-
-          <Form.Field>
-            <Form.Label htmlFor="phone">Phone</Form.Label>
-            <Form.InputWrapper>
-              <Form.InputIcon>
-                <FaPhone/>
-              </Form.InputIcon>
-              <Form.Input 
-                inputName="phone" 
-                placeholder="Your phone" 
-                inlineValidation 
-                mask={text => MaskInput('cellphone', text)}
-                validation={PhoneCheck}
-              />
-            </Form.InputWrapper>
-          </Form.Field>
-
-          <Form.Field>
-            <Form.Label htmlFor="name">Name</Form.Label>
-            <Form.InputWrapper>
-              <Form.InputIcon>
-                <FaUser/>
-              </Form.InputIcon>
-              <Form.Input inputName="name" placeholder="Your name" required />
-            </Form.InputWrapper>
-          </Form.Field>
-
-          <Form.ErrorMessage message="Can not send this Form" />
-
-          <Form.Button buttonText="Create account" />
-
-        </Form.Wrapper>
-      </div>
+      <h1 style={{ color: 'white' }}>Master</h1>
     </TestLayout>
   );
 }
