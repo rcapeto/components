231a7426550505dbe68ceffa61f1adc9c7d66be5
rcapeto <raphaelcapeto@gmail.com>
2023-03-08 16:52:38 -0300

    feat: slugify implementation

---
+++ src/utils/slugify.ts
@@ -0,0 +1,23 @@
+ export function slugify(text: string, separator: string = "-") {
+   if(typeof text !== 'string') {
+      console.warn("Text must be a string");
+      return;
+   }
+
+  return text
+    .replace(/\n/g, '')
+    .split(' ')
+    .map(data => data.trim().toLowerCase())
+    .filter(data => data)
+    .map(data => {
+      return isNaN(+data) ? removeSpecialCharAndWhiteSpace(data) : data;
+    })
+    .join(separator);
+ }
+
+ function removeSpecialCharAndWhiteSpace(word: string) {
+   return word
+     .normalize('NFD')
+     .replace(/[^a-zA-Z\s]/g, '')
+     .toLowerCase();
+ }
