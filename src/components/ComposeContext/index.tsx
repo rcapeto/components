6dc6a77fcc666a3ef375f2ae7c68b1402fd562f3
rcapeto <raphaelcapeto@gmail.com>
2023-03-15 14:23:40 -0300

    feature: Add context compose

---
+++ src/components/ComposeContext/index.tsx
@@ -0,0 +1,19 @@
+interface Props {
+   contexts: Array<React.JSXElementConstructor<React.PropsWithChildren<any>>>
+   children: React.ReactNode
+}
+
+export function ComposeContext(props: Props) {
+   const { children, contexts } = props;
+   
+   return (
+      <>
+         { 
+            contexts.reduceRight(
+               (acc, Context) => <Context>{acc}</Context>,
+               children
+            )
+         }
+      </>
+   );
+}
